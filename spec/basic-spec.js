89a9958b84c8eecb4ce97f450d6d15e65449a360
Stuart Knightley <stuart@stuartk.com>
2012-12-01 19:39:32 -0800

    Add failing spec for errback

--- spec/basic-spec.js
+++ spec/basic-spec.js
@@ -8,4 +8,10 @@ describe("promise-me basics", function() {
             promiseMe.convert("a(function(err, value){ console.log(value); })")
         ).toEqual("a().then(function (value) {\n    console.log(value);\n});");
     });
+
+    it("creates an errback if the error is checked", function() {
+        expect(
+            promiseMe.convert("a(function(err, value){ if(err) return; console.log(value); })")
+        ).toEqual("a().then(function (value) {\n    console.log(value);\n}, function(err) {\n    return;\n});");
+    });
 });
